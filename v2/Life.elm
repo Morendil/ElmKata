28587b458d655fe1fa623c070166db1358fb2fd7
Laurent Bossavit <laurent.git@bossavit.com>
2016-06-06 23:40:57 +0200

    Say something more interesting than hello

--- v2/Life.elm
+++ v2/Life.elm
@@ -1,13 +1,14 @@
 import Html exposing (text)
 import Html.App exposing (beginnerProgram)
 
-type alias Model = String
+type alias Coordinate = (Int, Int)
+type alias Model = (Int,Int,List Coordinate)
 
 model : Model
-model = "Hello World!"
+model = (1,1,[])
 
 view : Model -> Html.Html Never
-view model = text model
+view model = text (toString model)
 
 update : Never -> Model -> Model
 update event model = model
