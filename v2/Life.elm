e935d275445ac9259b674a49a341be40422dc306
Laurent Bossavit <laurent.git@bossavit.com>
2016-06-06 23:36:15 +0200

    Introduce other types

--- v2/Life.elm
+++ v2/Life.elm
@@ -3,11 +3,17 @@ import Html.App exposing (beginnerProgram)
 
 type alias Model = String
 
+model : Model
+model = "Hello World!"
+
+view : Model -> Html.Html Never
+view model = text model
+
 update : Never -> Model -> Model
 update event model = model
 
 main = beginnerProgram {
-    model  = "Hello World!",
-    view   = text,
+    model  = model,
+    view   = view,
     update = update
   }
