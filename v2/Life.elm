edbe2c2e109498c000ad5ccec822f37ac47e27d2
Laurent Bossavit <laurent.git@bossavit.com>
2016-06-06 23:46:21 +0200

    Let's talk about the Life domain

--- v2/Life.elm
+++ v2/Life.elm
@@ -1,14 +1,19 @@
 import Html exposing (text)
 import Html.App exposing (beginnerProgram)
+import String exposing (concat)
+import List exposing (length)
 
 type alias Coordinate = (Int, Int)
-type alias Model = (Int,Int,List Coordinate)
+type alias Dimensions = (Int, Int)
+type alias Age = Int
+type alias Model = (Age, Dimensions,List Coordinate)
 
 model : Model
-model = (1,1,[])
+model = (1,(1,1),[])
 
 view : Model -> Html.Html Never
-view model = text (toString model)
+view (age, (width, height), cells) = text <| concat
+    ["The world is ", toString age, " generations old and ", toString width," wide by ",toString height, " high, population: ",toString <| length cells]
 
 update : Never -> Model -> Model
 update event model = model
