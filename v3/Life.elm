0e24d7108edc3e7c49b5cd1118c7f931f8d39d00
Laurent Bossavit <laurent.git@bossavit.com>
2016-06-08 16:14:24 +0200

    Rename our type to be more explicit

--- v3/Life.elm
+++ v3/Life.elm
@@ -3,20 +3,20 @@ import Html.App exposing (beginnerProgram)
 import Html.Events exposing (onClick)
 import String exposing (concat)
 
-type alias Model = Int
+type alias World = Int
 type Event = Start
 
-model : Model
+model : World
 model = 0
 
-view : Model -> Html Event
+view : World -> Html Event
 view model =
     div [] [
         text (String.concat ["The world is new! It's ", (toString model), " seconds old."]),
         div [onClick Start] [text "Click to start the world."]
     ]
 
-update : Event -> Model -> Model
+update : Event -> World -> World
 update event model = model + 1
 
 main = beginnerProgram {
