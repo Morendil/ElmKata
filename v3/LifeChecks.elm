4db32206322dbdf8061fe60b3391c3fdd8dc6eda
Laurent Bossavit <laurent.git@bossavit.com>
2016-06-08 19:42:08 +0200

    Introduce unit tests

---
+++ v3/LifeChecks.elm
@@ -0,0 +1,11 @@
+import ElmTest exposing (runSuiteHtml, equals)
+
+import Life exposing (dimensions, model)
+
+myTests = ElmTest.suite "Tests"
+  [
+    let model' = {model | w=20, h=10}
+    in equals {w=200.0, h=200.0} (dimensions model')
+  ]
+
+main = ElmTest.runSuiteHtml myTests
