4cea3e245a6bbe45d2d372cff39953cd94fbfbdc
Laurent Bossavit <laurent.git@bossavit.com>
2016-06-08 21:40:16 +0200

    Grab window size on startup

--- v3/Life.elm
+++ v3/Life.elm
@@ -7,13 +7,15 @@ import Element exposing (toHtml)
 import Collage exposing (filled, rect, collage)
 import Color exposing (..)
 import String exposing (concat)
+import Task exposing (perform)
+import Window exposing (resizes, size, Size)
 
 type alias Dimensions = {w:Float, h:Float}
-type alias World = {w:Float, h:Float, age: Int}
-type Event = Start
+type alias World = {win:Size, w:Float, h:Float, age: Int}
+type Event = Start | Window Size
 
 model : World
-model = {w=20,h=20,age=0}
+model = {win={width=0,height=0},w=30,h=30,age=0}
 
 dimensions: World -> Dimensions
 dimensions world = {w=10*world.w, h=10*world.h}
@@ -26,15 +28,20 @@ view model =
         text (String.concat ["The world is new! It's ", (age model), " seconds old."]),
         div [onClick Start] [text "Click to start the world."],
         let dim = dimensions model
-        in toHtml <| collage 100 100 [filled black <| rect dim.w dim.h]
+        in toHtml <| collage model.win.width model.win.height [filled black <| rect dim.w dim.h]
     ]
 
 update : Event -> World -> World
-update event model = {model | age = model.age + 1}
+update event model =
+    case event of
+        Start -> {model | age = model.age + 1}
+        Window size -> {model | win = size}
+
+subscriptions = always (resizes Window)
 
 main = program {
-        init = (model, Cmd.none),
+        init = (model, perform (always Start) Window size),
         view = view,
         update = \ event model -> (update event model, Cmd.none),
-        subscriptions = \ model -> Sub.none
+        subscriptions = subscriptions
     }
