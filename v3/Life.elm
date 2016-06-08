52527fdd7b55e159ef9cd0873c6709f6d00c0e0c
Laurent Bossavit <laurent.git@bossavit.com>
2016-06-08 21:16:56 +0200

    Change over to program (and fix age)

--- v3/Life.elm
+++ v3/Life.elm
@@ -1,7 +1,7 @@
 module Life exposing (..) -- where
 
 import Html exposing (a, div, text, Html)
-import Html.App exposing (beginnerProgram)
+import Html.App exposing (program)
 import Html.Events exposing (onClick)
 import Element exposing (toHtml)
 import Collage exposing (filled, rect, collage)
@@ -18,19 +18,23 @@ model = {w=20,h=20,age=0}
 dimensions: World -> Dimensions
 dimensions world = {w=10*world.w, h=10*world.h}
 
+age model = toString <| model.age
+
 view : World -> Html Event
 view model =
     div [] [
-        text (String.concat ["The world is new! It's ", (toString model), " seconds old."]),
+        text (String.concat ["The world is new! It's ", (age model), " seconds old."]),
         div [onClick Start] [text "Click to start the world."],
-        toHtml <| collage 100 100 [filled black <| rect 100.0 100.0]
+        let dim = dimensions model
+        in toHtml <| collage 100 100 [filled black <| rect dim.w dim.h]
     ]
 
 update : Event -> World -> World
 update event model = {model | age = model.age + 1}
 
-main = beginnerProgram {
-        model = model,
+main = program {
+        init = (model, Cmd.none),
         view = view,
-        update = update
+        update = \ event model -> (update event model, Cmd.none),
+        subscriptions = \ model -> Sub.none
     }
