00d4b9919b79b05fc3ed7b74708ca808aef10482
Laurent Bossavit <laurent.git@bossavit.com>
2016-06-08 16:00:26 +0200

    Refactor, extracting functions

--- v3/Life.elm
+++ v3/Life.elm
@@ -2,8 +2,12 @@ import Html exposing (text)
 import Html.App exposing (beginnerProgram)
 import String exposing (concat)
 
+model = 0
+view model = text (String.concat ["The world is new! It's ", (toString model), " seconds old."])
+update event model = model
+
 main = beginnerProgram {
-        model = 0,
-        view = \ model -> text (String.concat ["The world is new! It's ", (toString model), " seconds old."]),
-        update = \ event model -> model
+        model = model,
+        view = view,
+        update = update
     }
