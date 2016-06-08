8b24e626502d150a4191b99dece0f85317195b25
Laurent Bossavit <laurent.git@bossavit.com>
2016-06-08 16:10:49 +0200

    Introduce an event to start the world

--- v3/Life.elm
+++ v3/Life.elm
@@ -1,16 +1,22 @@
-import Html exposing (text, Html)
+import Html exposing (a, div, text, Html)
 import Html.App exposing (beginnerProgram)
+import Html.Events exposing (onClick)
 import String exposing (concat)
 
 type alias Model = Int
+type Event = Start
 
 model : Model
 model = 0
 
-view : Model -> Html Never
-view model = text (String.concat ["The world is new! It's ", (toString model), " seconds old."])
+view : Model -> Html Event
+view model =
+    div [] [
+        text (String.concat ["The world is new! It's ", (toString model), " seconds old."]),
+        div [onClick Start] [text "Click to start the world."]
+    ]
 
-update : Never -> Model -> Model
+update : Event -> Model -> Model
 update = always identity
 
 main = beginnerProgram {
