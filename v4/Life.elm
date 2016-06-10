7cd24432871de0833f32c545e6be039e88d7793a
Laurent Bossavit <laurent.git@bossavit.com>
2016-06-10 23:24:19 +0200

    Compute cell neighbors

--- v4/Life.elm
+++ v4/Life.elm
@@ -1,27 +1,35 @@
-module Life exposing (view, evolve) -- where
+module Life exposing (view, evolve, neighbours) -- where
 import Html exposing (text)
 import Html.App exposing (program)
 import String exposing (concat)
-import List exposing (length)
+import List exposing (length, map, concatMap, drop)
 import Time exposing (Time, second)
 
 type Event = Tick Time
 
 world = [(0,0)]
 
+evolve word = []
+
+neighbours (x,y) =
+    map (\ (ox,oy) -> (x+ox,y+oy)) neighbourOffsets
+
+neighbourOffsets =
+    let offsets = [0,1,-1]
+        pairWith offsets x = map ((,) x) offsets
+    in drop 1 <| concatMap (pairWith offsets) offsets
+
 view cells =
     case cells of
         []          -> text "The world is empty"
         otherwise   -> text <| concat ["The world has population ", toString <| length cells, " ", toString cells]
-
-evolve word = []
 
 update event model = (evolve model, Cmd.none)
 
 main = program
     {
     init = (world, Cmd.none),
-    view = \ model -> view model,
+    view = view,
     update = update,
-    subscriptions = \ model -> Time.every second Tick
+    subscriptions = always (Time.every second Tick)
     }
--- v4/LifeChecks.elm
+++ v4/LifeChecks.elm
@@ -1,12 +1,14 @@
 import ElmTest exposing (runSuiteHtml, suite, equals)
 import Html exposing (text)
-import Life exposing (view, evolve)
+import Life exposing (view, evolve, neighbours)
 
 myTests = suite "Life"
     [
         equals (text "The world is empty")  (view []),
         equals (text "The world has population 1 [(0,0)]")  (view [(0,0)]),
-        equals [] (evolve [(0,0)])
+        equals [] (evolve [(0,0)]),
+        equals [(0,1),(0,-1),(1,0),(1,1),(1,-1),(-1,0),(-1,1),(-1,-1)] (neighbours (0,0)),
+        equals [(4,5),(4,3),(5,4),(5,5),(5,3),(3,4),(3,5),(3,3)] (neighbours (4,4))
     ]
 
 main = runSuiteHtml myTests
