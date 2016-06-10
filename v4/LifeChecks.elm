22a665fcf09753f0153c6d3ee28655201d590239
Laurent Bossavit <laurent.git@bossavit.com>
2016-06-10 18:44:54 +0200

    A second failing test

--- v4/LifeChecks.elm
+++ v4/LifeChecks.elm
@@ -2,6 +2,10 @@ import ElmTest exposing (runSuiteHtml, suite, equals)
 import Html exposing (text)
 import Life exposing (view)
 
-myTests = suite "Life" [equals (text "The world is empty")  (view [])]
+myTests = suite "Life"
+    [
+        equals (text "The world is empty")  (view []),
+        equals (text "The world has population 1 [(0,0])")  (view [(0,0)])
+    ]
 
 main = runSuiteHtml myTests
