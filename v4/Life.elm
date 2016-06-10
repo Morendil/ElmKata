936386fbda3238b53eafc80d3a171575c74ea2b2
Laurent Bossavit <laurent.git@bossavit.com>
2016-06-10 18:46:47 +0200

    Make the test pass

--- v4/Life.elm
+++ v4/Life.elm
@@ -1,8 +1,13 @@
 module Life exposing (view) -- where
 import Html exposing (text)
+import String exposing (concat)
+import List exposing (length)
 
 world = [(0,0)]
 
-view world = text <| toString world
+view cells =
+    case cells of
+        []          -> text "The world is empty"
+        otherwise   -> text <| concat ["The world has population ", toString <| length cells, " ", toString cells]
 
 main = view world
--- v4/LifeChecks.elm
+++ v4/LifeChecks.elm
@@ -5,7 +5,7 @@ import Life exposing (view)
 myTests = suite "Life"
     [
         equals (text "The world is empty")  (view []),
-        equals (text "The world has population 1 [(0,0])")  (view [(0,0)])
+        equals (text "The world has population 1 [(0,0)]")  (view [(0,0)])
     ]
 
 main = runSuiteHtml myTests
