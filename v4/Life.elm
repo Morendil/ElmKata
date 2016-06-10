2f359edbfc82ef12c225843079e274cf0e226baa
Laurent Bossavit <laurent.git@bossavit.com>
2016-06-10 22:59:36 +0200

    Fake it

--- v4/Life.elm
+++ v4/Life.elm
@@ -1,4 +1,4 @@
-module Life exposing (view) -- where
+module Life exposing (view, evolve) -- where
 import Html exposing (text)
 import String exposing (concat)
 import List exposing (length)
@@ -9,5 +9,7 @@ view cells =
     case cells of
         []          -> text "The world is empty"
         otherwise   -> text <| concat ["The world has population ", toString <| length cells, " ", toString cells]
+
+evolve word = []
 
 main = view world
--- v4/LifeChecks.elm
+++ v4/LifeChecks.elm
@@ -1,11 +1,11 @@
 import ElmTest exposing (runSuiteHtml, suite, equals)
 import Html exposing (text)
-import Life exposing (view)
+import Life exposing (view, evolve)
 
 myTests = suite "Life"
     [
         equals (text "The world is empty")  (view []),
-        equals (text "The world has population 1 [(0,0)]")  (view [(0,0)])
+        equals (text "The world has population 1 [(0,0)]")  (view [(0,0)]),
         equals [] (evolve [(0,0)])
     ]
 
