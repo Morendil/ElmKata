6033b96b14753f23f94b2ec2850b4d94eb9ddc3a
Laurent Bossavit <laurent.git@bossavit.com>
2016-06-11 11:53:11 +0200

    Blinker test

--- v4/LifeChecks.elm
+++ v4/LifeChecks.elm
@@ -10,7 +10,8 @@ myTests = suite "Life"
         equals [(0,1),(0,-1),(1,0),(1,1),(1,-1),(-1,0),(-1,1),(-1,-1)] (neighbours (0,0)),
         equals [(4,5),(4,3),(5,4),(5,5),(5,3),(3,4),(3,5),(3,3)] (neighbours (4,4)),
         equals 1 (countNeighbours (0,0) [(0,1)]),
-        equals 8 (countNeighbours (0,0) (neighbours (0,0)))
+        equals 8 (countNeighbours (0,0) (neighbours (0,0))),
+        equals [(-1,0),(0,0),(1,0)] (evolve [(0,-1),(0,0),(0,1)])
     ]
 
 main = runSuiteHtml myTests
